1bbb202d83167e7462eb9e9fa538869959edb498
antour <mrayhi.a@gmail.com>
2019-08-28 15:22:25 +0900

    v2

--- EWM-SMS/lambda.js
+++ EWM-SMS/lambda.js
@@ -1,10 +1,34 @@
 let AWS = require('aws-sdk');
-exports.handler = function(event, context, callback) {
-     let receiver = event['receiver'];
+const sns = new AWS.SNS();
+
+exports.handler = function (event, context, callback) {
+    let receiver = event['receiver'];
     let sender = event['sender'];
     let message = event['message'];
-
+    let isPromotional = true;
     console.log("Sending message", message, "to receiver", receiver);
+    sns.publish({
+        Message: message,
+        MessageAttributes: {
+            'AWS.SNS.SMS.SMSType': {
+                'DataType': 'String',
+                'StringValue': 'Promotional'
+            },
+            'AWS.SNS.SMS.SenderID': {
+                'DataType': 'String',
+                'StringValue': sender
+            }
+        },
+        PhoneNumber: receiver
+    }).promise()
+        .then(data => {
+            console.log("Sent message to", receiver);
+			callback(null, data);
+        })
+        .catch(err => {
+            console.log("Sending failed", err);
+			callback(err);
+            // error handling goes here
+        });
 
-    callback(null,'Successfully executed')
-}
+   }
